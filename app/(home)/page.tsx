2500c5e28bf9fbaad1e0fa0291db9b7cc4eaad67
Kauê <alexkauezinho@gmail.com>
2024-01-31 18:55:12 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,18 @@
-import Image from "next/image";
+import { ptBR } from "date-fns/locale";
 import Header from "../_components/header";
+import {format} from 'date-fns'
 
 export default function Home() {
   return (
     <div>
       <Header/>
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, User!</h2>
+        {/* date fns */}
+        <p className="capitalize text-sm">
+        {format(new Date(),"EEEE', 'dd' de ' MMMM", {locale: ptBR})}
+        </p>
+      </div>
     </div>
    
   );
