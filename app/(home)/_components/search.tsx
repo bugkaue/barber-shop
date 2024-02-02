bbfb45090cdcb4c9c87157724f59ab26af9657e2
Kauê <alexkauezinho@gmail.com>
2024-02-02 14:26:48 -0300

    feat: add welcome mensage

---
+++ app/(home)/_components/search.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/app/_components/ui/button";
+import { Input } from "@/app/_components/ui/input";
+
+const Search = () => {
+    return ( 
+        <div className="flex items-center" >
+            <Input />
+        </div>
+     );
+}
+ 
+export default Search;
